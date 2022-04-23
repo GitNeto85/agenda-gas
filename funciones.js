16200798b2f722d4db02aa29e3acda494b693e4a
GitNeto85 <jesus_ernesto_g@hotmail.com>
2022-04-23 10:09:40 -0500

    video 9 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -7,3 +7,10 @@ function obtenerDatosHTML(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos()
+{
+    let hoja = SpreadsheetApp.openById('12MhlFYwGpxh1Aivef851ifWsnEcGt3m8bcRtK7Ek9zQ').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
