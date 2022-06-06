74b2316fd128179beadeb143bc027e5ce8e3897a
GitNeto85 <jesus_ernesto_g@hotmail.com>
2022-06-06 09:49:21 -0500

    video29 gas avanzado

--- funciones.js
+++ funciones.js
@@ -1,55 +1,71 @@
 const HOJA = SpreadsheetApp.openById('12MhlFYwGpxh1Aivef851ifWsnEcGt3m8bcRtK7Ek9zQ').getActiveSheet();
 const CARPETA = DriveApp.getFolderById('1ZJ1xmFD-6bTceEd8V9wyRieYnlOuIXCY');
-const CABECERA_URL_IMAGEN_DRIVE = 'https://drive.google.com/uc?export=view&id='
+const CABECERA_URL_IMAGEN_DRIVE = 'https://drive.google.com/uc?export=view&id=';
 
-function doGet(datos) {
+function doGet(datos)
+{    
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Script');
 }
 
-function doPost(datos) {
+function doPost(datos)
+{    
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Script');
 }
 
-function obtenerDatosHTML(nombre) {
+function obtenerDatosHTML(nombre)
+{
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
 
-function obtenerDatos() {
-    return HOJA.getDataRange().getValues();
+function obtenerDatos()
+{
+    return HOJA.getDataRange().getValues();    
 }
 
-function insertarContacto(contacto, imagen) {
-    if (imagen) contacto.imagen = guardarImagen(imagen);
+function insertarContacto(contacto, imagen)
+{
+    if(imagen) contacto.imagen = guardarImagen(imagen);
     HOJA.appendRow([contacto.nombre, contacto.apellidos, contacto.correo, contacto.telf, contacto.imagen]);
 }
 
-function modificarContacto(contacto, imagen) {
-
-    if (imagen) contacto.imagen = guardarImagen(imagen);
-    let celdas = HOJA.getRange('A' + contacto.fila + ':E' + contacto.fila);
+function modificarContacto(contacto, imagen)
+{
+    if(imagen) contacto.imagen = guardarImagen(imagen);
+    let celdas = HOJA.getRange('A'+contacto.fila+':E'+contacto.fila);
     celdas.setValues([[contacto.nombre, contacto.apellidos, contacto.correo, contacto.telf, contacto.imagen]]);
 }
 
-function importarContactos() {
+function guardarImagen(imagen)
+{
+    let blob = Utilities.newBlob(imagen.datos, imagen.tipo, imagen.nombre);
+    let archivo = CARPETA.createFile(blob);
+    return CABECERA_URL_IMAGEN_DRIVE+archivo.getId();
+}
+
+function borrarContacto(numFila)
+{
+    HOJA.deleteRow(numFila);
+}
+
+function importarContactos()
+{
     let url = 'https://randomuser.me/api/?results=5&inc=name,email,phone,picture';
     let respuesta = UrlFetchApp.fetch(url).getContentText();
     let datos = JSON.parse(respuesta);
 
-    datos.results.forEach(insertarContactoJSON);
-
+    datos.results.forEach(insertarContactoJSON);     
 }
 
-function guardarImagen(imagen) {
-    let blob = Utilities.newBlob(imagen.datos, imagen.tipo, imagen.nombre);
-        let archivo = CARPETA.createFile(blob);
-        return CABECERA_URL_IMAGEN_DRIVE + archivo.getId();
+function insertarContactoJSON(contacto)
+{
+  HOJA.appendRow([contacto.name.first, contacto.name.last, contacto.email, contacto.phone, contacto.picture.large]);
 }
 
-function borrarContacto(numFila) {
-    HOJA.deleteRow(numFila);
-}
 
-function insertarContactoJSON(contacto) {
-    HOJA.appendRow([contacto.name.first, contacto.name.last, contacto.email, contacto.phone, contacto.picture.large]);
-}
+
+
+
+
+
+
 
